2adf560b3eeb46285d9f28038731e94d20a75815
Вячеслав Смирнов <smslava7@gmail.com>
2025-10-11 13:17:05 +0300

    Фундамент меню

--- AthleteLink/pages/views.py
+++ AthleteLink/pages/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+def default_menu() -> tuple:
+    """
+    Кортеж с текстом и url для каждой ссылки в менюшке
+    """
+    menu = (
+        {'url': '/', 'text': 'Главная'},
+    )
+    return menu
 
 def index_page(request: HttpRequest) -> HttpResponse:
-    urls = (
-        {'text': 'Регистрация', 'url': '/register'},
-        {'text': 'Авторизация', 'url': '/login'},
-        {'text': 'Смешная кнопка', 'url': '/lol'}
-    )
     context = {
         'page_name': 'AthleteLink',
-        'urls': urls
+        'menu': default_menu()
     }
     return render(request, 'pages/index.html', context)
 
