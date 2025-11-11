7199836ce3ee7550a0e57a52f090467ed220d0eb
Sergey Belkin <sevbelkin@mai.education>
2025-11-11 16:41:41 +0300

    refactoring(раскидал все по папкам)

--- AthleteLink/user/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def register(request):
-    return HttpResponse("Страница регистрации (в разработке)")
-
-def user_login(request):
-    return HttpResponse("Страница входа (в разработке)")
