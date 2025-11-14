6072840977bf7a2a41cadfa74d75579fb067a257
Вячеслав Смирнов <smslava7@gmail.com>
2025-11-14 16:23:43 +0300

    Удалил ненужное

--- AthleteLink/requests/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'requests'
-
-urlpatterns = [
-    path("", views.requests_page, name='requests')
-]
--- AthleteLink/requests/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def requests_page(request):
-    return render(request, 'requests/requests_page.html')
