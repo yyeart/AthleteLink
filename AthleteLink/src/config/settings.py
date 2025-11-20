b6f8c65f52a6eff187e69caf170ed12d1d68f15d
Вячеслав Смирнов <smslava7@gmail.com>
2025-11-20 23:11:47 +0300

    lil fix

--- AthleteLink/src/config/settings.py
+++ AthleteLink/src/config/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
 
