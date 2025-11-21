8fadd931bc42acf0fdab14bfbdfbdd77c6c29901
Вячеслав Смирнов <smslava7@gmail.com>
2025-11-21 23:52:52 +0300

    tiny fix

--- AthleteLink/src/app/requests/urls.py
+++ AthleteLink/src/app/requests/urls.py
@@ -5,5 +5,5 @@ app_name = 'requests'
 
 urlpatterns = [
     path("", views.RequestListAPIView.as_view(), name='list'),
-    path("/create/", views.create_request_page, name='create')
+    # path("/create/", views.create_request_page, name='create') ПОКА НЕ ВОРКАЕТ
 ]
